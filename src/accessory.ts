c71062cf103eec3c9fb138a9027b2823a7864fb3
ariesb <ariesbe@icloud.com>
2021-04-27 13:30:56 +0800

    Fixed issue on updates

--- src/accessory.ts
+++ src/accessory.ts
@@ -65,7 +65,7 @@ class AirPurifierDevice {
     model: "zhimi.airpurifier.mb4",
     firmware: "UNKNOWN"
   };
-  private callbacks: any[] = [];
+  private callbacks: Function = () => { };
 
   constructor(config: DeviceConfig, log: Logging) {
     this.config = config;
@@ -85,7 +85,7 @@ class AirPurifierDevice {
   }
 
   getFirmware() {
-    return this.deviceInfo.firmware;
+    return this.deviceInfo.firmware || "0.1.0";
   }
 
   trackProperty(siid: number, piid: number) {
@@ -97,24 +97,23 @@ class AirPurifierDevice {
     });
   }
 
-  onChange(charateristics: string, callback: Function) {
-    this.callbacks.push({
-      charateristics,
-      callback
-    })
+  onChange(callback: Function) {
+    this.callbacks = callback;
   }
 
   private getProperty(props: any, siid: number, piid: number): any {
-    return props.filter((p: any) => (p.siid === siid && p.piid === piid));
+    let val = props.filter((p: any) => (p.siid === siid && p.piid === piid));
+    return val ? val[0] : { value: undefined };
   }
 
   setDeviceCharateristics(props: any) {
+    let oldPowerValue = this.deviceCharateristics.power;
+    let oldAQIValue = this.deviceCharateristics.aqi;
     this.deviceCharateristics.power = this.getProperty(props, 2, 1).value;
     this.deviceCharateristics.aqi = this.getProperty(props, 3, 4).value;
 
-    this.callbacks.forEach(calls => {
-      calls();
-    });
+    if (oldPowerValue != this.deviceCharateristics.power ||
+      oldAQIValue != this.deviceCharateristics.aqi) this.callbacks();
   }
 
   getDeviceCharacteristics(): DeviceCharacteristics {
@@ -141,16 +140,18 @@ class AirPurifierDevice {
         }])
       this.updateProperties();
     } catch (deviceError) {
-      this.log.error("Device failure: ", deviceError.code);
+      this.log.error("Device powerSwitch failure: ", deviceError.code);
     }
   }
 
   async connect() {
     try {
       this.device = await miio.device({ address: this.config.ip, token: this.config.token });
+      this.log(`Device ${this.config.did} is now connected.`);
       let info = await this.device.miioCall("miIO.info");
+      this.log(`Device Firmware is ${info.fw_ver}.`);
       this.deviceInfo.firmware = info.fw_ver;
-      this.updateProperties();
+      await this.updateProperties();
     } catch (connectionError) {
       this.log.error("Device connection failure: ", connectionError.code);
       setTimeout(function (self) {
@@ -165,7 +166,6 @@ class XiaomiAirPurifier3CAccessory implements AccessoryPlugin {
   private readonly log: Logging;
   private readonly name: string;
   private readonly breakpoints: number[];
-  private switchOn = false;
   private device: AirPurifierDevice;
 
   private readonly airPurifierService: Service;
@@ -183,9 +183,7 @@ class XiaomiAirPurifier3CAccessory implements AccessoryPlugin {
       ip: config.ip
     };
     this.device = new AirPurifierDevice(deviceConfig, log);
-
-    this.device.onChange('power', this.onPowerChange.bind(this));
-    this.device.onChange('aqi', this.onAirQualityChange.bind(this));
+    this.device.onChange(this.onPropsChange.bind(this));
 
     this.informationService = new hap.Service.AccessoryInformation()
       .setCharacteristic(hap.Characteristic.Name, this.name)
@@ -199,11 +197,17 @@ class XiaomiAirPurifier3CAccessory implements AccessoryPlugin {
       .getCharacteristic(hap.Characteristic.Active)
       .on(CharacteristicEventTypes.GET, this.getPower.bind(this))
       .on(CharacteristicEventTypes.SET, this.setPower.bind(this));
+    this.airPurifierService
+      .getCharacteristic(hap.Characteristic.CurrentAirPurifierState)
+      .on(CharacteristicEventTypes.GET, this.getCurrentAirPurifierState.bind(this));
 
     this.airQualitySensorService = new hap.Service.AirQualitySensor(this.name);
     this.airQualitySensorService
       .getCharacteristic(hap.Characteristic.AirQuality)
       .on(CharacteristicEventTypes.GET, this.getAirQuality.bind(this));
+    this.airQualitySensorService
+      .getCharacteristic(hap.Characteristic.StatusActive)
+      .on(CharacteristicEventTypes.GET, this.getPower.bind(this));
 
     setInterval(this.pollProperties.bind(this), 30000);
 
@@ -257,6 +261,13 @@ class XiaomiAirPurifier3CAccessory implements AccessoryPlugin {
     }
   }
 
+  onPropsChange() {
+    this.onPowerChange();
+    this.onAirQualityChange();
+    this.onCurrentAirPurifierStateChange();
+    this.informationService.setCharacteristic(hap.Characteristic.FirmwareRevision, this.device.getFirmware());
+  }
+
   onPowerChange() {
     this.log('onPowerChage');
 
@@ -272,17 +283,6 @@ class XiaomiAirPurifier3CAccessory implements AccessoryPlugin {
     }
   }
 
-  private getAirQualityCharacteristic(aqi: number) {
-    let quality = hap.Characteristic.AirQuality.UNKNOWN;
-    if (aqi <= this.breakpoints[0]) { quality = hap.Characteristic.AirQuality.EXCELLENT; }
-    else if (aqi <= this.breakpoints[1]) { quality = hap.Characteristic.AirQuality.GOOD; }
-    else if (aqi <= this.breakpoints[2]) { quality = hap.Characteristic.AirQuality.FAIR; }
-    else if (aqi <= this.breakpoints[3]) { quality = hap.Characteristic.AirQuality.INFERIOR; }
-    else { quality = hap.Characteristic.AirQuality.POOR; }
-
-    return quality;
-  }
-
   onAirQualityChange() {
     this.log("onAirQualityChange");
 
@@ -295,7 +295,18 @@ class XiaomiAirPurifier3CAccessory implements AccessoryPlugin {
     }
   }
 
-  getAirQuality(callback: Function) {
+  private getAirQualityCharacteristic(aqi: number) {
+    let quality = hap.Characteristic.AirQuality.UNKNOWN;
+    if (aqi <= this.breakpoints[0]) { quality = hap.Characteristic.AirQuality.EXCELLENT; }
+    else if (aqi <= this.breakpoints[1]) { quality = hap.Characteristic.AirQuality.GOOD; }
+    else if (aqi <= this.breakpoints[2]) { quality = hap.Characteristic.AirQuality.FAIR; }
+    else if (aqi <= this.breakpoints[3]) { quality = hap.Characteristic.AirQuality.INFERIOR; }
+    else { quality = hap.Characteristic.AirQuality.POOR; }
+
+    return quality;
+  }
+
+  getAirQuality(callback: CharacteristicGetCallback) {
     try {
       let aqi = this.device.getDeviceCharacteristics().aqi;
       this.log("getAirQuality: " + aqi);
@@ -306,4 +317,35 @@ class XiaomiAirPurifier3CAccessory implements AccessoryPlugin {
       callback(e);
     }
   }
+
+  getCurrentAirPurifierState(callback: CharacteristicGetCallback) {
+    this.log('getCurrentAirPurifierState');
+    try {
+      var value = hap.Characteristic.CurrentAirPurifierState.INACTIVE;
+
+      if (this.device.getDeviceCharacteristics().power == true) {
+        value = (this.device.getDeviceCharacteristics().motorSpeed == 0) ? hap.Characteristic.CurrentAirPurifierState.IDLE : hap.Characteristic.CurrentAirPurifierState.PURIFYING_AIR;
+      }
+      callback(null, value);
+    } catch (e) {
+      this.log('getCurrentAirPurifierState Failed: ' + e);
+      callback(e);
+    }
+  }
+
+  onCurrentAirPurifierStateChange() {
+    this.log('onCurrentAirPurifierStateChange');
+
+    try {
+      var value = hap.Characteristic.CurrentAirPurifierState.INACTIVE;
+
+      if (this.device.getDeviceCharacteristics().power == true) {
+        value = (this.device.getDeviceCharacteristics().motorSpeed == 0) ? hap.Characteristic.CurrentAirPurifierState.IDLE : hap.Characteristic.CurrentAirPurifierState.PURIFYING_AIR;
+      }
+
+      this.airPurifierService.setCharacteristic(hap.Characteristic.CurrentAirPurifierState, value);
+  } catch(e) {
+      this.log('onCurrentAirPurifierStateChange Failed: ' + e);
+  }
+}
 }
